85715eb1f38f5d3e869f6095b2b1a9deca82f76a
Steven Miyakawa SAM <samprofessional@gmail.com>
2014-03-02 12:57:09 -0800

    Boilerplate code

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TradeOff::Application.config.secret_key_base = '6fde884bcb01c6aee985431c6a86cf2796f23233658fe6e14b1761b44a95a885a6afc8524ba9d9d5024249cb570381a88789f5bd5edc08066101a8cb02158b83'
